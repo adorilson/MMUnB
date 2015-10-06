2abdb3670d14fca88e049a8fbbe4a246f45a52aa
Adorilson Bezerra <adorilson@ppgsc.ufrn.br>
2015-10-06 19:14:15 -0300

    Running MainActivityIsFragmentActivity check with alert

--- src/br/unb/mobileMedia/MMUnBActivity.java
+++ src/br/unb/mobileMedia/MMUnBActivity.java
@@ -6,31 +6,46 @@ import br.unb.mobileMedia.core.view.AudioPlayerFragment;
 import br.unb.mobileMedia.core.view.AuthorListFragment;
 import br.unb.mobileMedia.core.view.ShareListFragment;
 import br.unb.mobileMedia.playlist.MainPlaylistListFragment;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+
+import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
-public class MMUnBActivity extends FragmentActivity implements OnItemClickedCallBack{
+public class MMUnBActivity extends Activity implements OnItemClickedCallBack{
+
+    private void configLayout(){
+        MenuFragment menuFrag = new MenuFragment();
+
+/*
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (findViewById(R.id.main)!=null){
+            transaction.add(R.id.main, menuFrag);
+        }else{
+            transaction.add(R.id.menu, menuFrag);
+            transaction.add(R.id.content, new ContentFragment());
+        }
+
+        transaction.commit();
+*/
+    }
 
 	@Override
-	protected void onCreate(Bundle savedInstanceStace){
+	protected void onCreate(final Bundle savedInstanceStace){
 		super.onCreate(savedInstanceStace);
 		setContentView(R.layout.main);
-		
-		MenuFragment menuFrag = new MenuFragment();
-		
-		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-		if (findViewById(R.id.main)!=null){
-			transaction.add(R.id.main, menuFrag);
-		}else{
-			transaction.add(R.id.menu, menuFrag);
-			transaction.add(R.id.content, new ContentFragment());
-		}
-		transaction.commit();
 
+        configLayout();
+
+
+        //FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 	}
 	
 	public void onItemClicked(int menuItem){
@@ -63,10 +78,13 @@ public class MMUnBActivity extends FragmentActivity implements OnItemClickedCall
 			default:
 				Toast.makeText(getApplicationContext(), R.string.need_to_be_implemented, Toast.LENGTH_LONG).show();
 		}
+
 		// TODO Extract this to a method (repeated in AuthorListFragment too)
+
 		if (newFragment !=null){
-			FragmentManager manager = getSupportFragmentManager();
-			FragmentTransaction transaction = manager.beginTransaction();
+            /*
+			FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
 			
 			if(findViewById(R.id.main) != null){
 				transaction.replace(R.id.main, newFragment);
@@ -75,8 +93,16 @@ public class MMUnBActivity extends FragmentActivity implements OnItemClickedCall
 				transaction.replace(R.id.content, newFragment);
 			}
 			transaction.commit();
+			*/
 		}
-		
 	}
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Toast.makeText(getApplicationContext(), R.string.need_to_be_implemented, Toast.LENGTH_LONG).show();
+        return super.onTouchEvent(event);
+    }
+
+
 }
+
